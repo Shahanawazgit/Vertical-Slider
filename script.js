103d2b0ff93a1d1431772bfd0d16fb611e12415c
Shahanawaz <shahanawazkhan579@gmail.com>
2023-07-20 18:48:57 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const sliderContainer = document.querySelectorAll(".slider_container");
+const slideLeft = document.querySelector(".left_slide");
+const slideRight = document.querySelector(".right_slide");
+const upButton = document.querySelector(".up_button");
+const downButton = document.querySelector(".down_button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => {
+  changeSlide("up");
+});
+downButton.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+const changeSlide = (direction) => {
+  const sliderHeight = slideRight.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
